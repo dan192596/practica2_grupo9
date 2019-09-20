3e94eb8e6ff60f11fae52736b1d839a5a0b57c9f
Daniel Perez <dan192596@gmail.com>
2019-09-20 12:24:06 -0600

    Creacion de test Url Lista Clientes

--- ClinicaMedica/tests.py
+++ ClinicaMedica/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
 # Create your tests here.
+def test_ListaClientes_is_resolved(self):
+    url = reverse('cita:ListaClientes')
+    self.assertEquals(resolve(url).url_name,'ListaClientes')
