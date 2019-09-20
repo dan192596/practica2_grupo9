4ed3cd880eefc137d202fa40dc2eb5402eebd99b
gary <gortiz1490@gmail.com>
2019-09-19 21:24:50 -0600

    Modelo Login
    se creo la vista form y modelos para la historia login

--- ClinicaMedica/models.py
+++ ClinicaMedica/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+#Modelo para el login
+class ClientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
--- ClinicaMedica/urls.py
+++ ClinicaMedica/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
+app_name = 'CM'
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^registro/$', views.register, name='registro'),
+    url(r'^perfil/$', views.index, name='perfil'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^$', LoginView.as_view(template_name='login.html'), name="login"),
 ]
